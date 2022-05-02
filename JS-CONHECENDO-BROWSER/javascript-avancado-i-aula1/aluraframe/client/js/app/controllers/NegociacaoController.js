be854d43131cf14e0c54f1edb112f37adb6b0c79
Drospe <pedro.jtrudes@gmail.com>
2022-05-02 12:02:32 -0300

    Subindo MVC para implementar no projeto

--- JS-CONHECENDO-BROWSER/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JS-CONHECENDO-BROWSER/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,13 +7,25 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacaos();
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+        
+        this._mensagem = new  Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
     
     adiciona(event) {
         
         event.preventDefault();
-        
+
         this._listaNegociacoes.adiciona(this._CriaNegociacao());
+
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagemView.update(this._mensagem);
+
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
         console.log(this._listaNegociacoes.negociacoes);
     
